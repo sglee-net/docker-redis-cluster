762f7b93d5a1bd798364985c79861dcc39c9739f
SG Lee <33934527+sglee-net@users.noreply.github.com>
2020-03-24 21:54:36 +0900

    Update create-redis-cluster.sh
    마스터/슬레이브 클러스터 구성 방안 참조 추가

--- create-redis-cluster.sh
+++ create-redis-cluster.sh
@@ -3,3 +3,4 @@
 # 참고사이트: https://daddyprogrammer.org/post/1601/redis-cluster/
 
 echo yes | sudo docker exec -i $1 redis-cli -h 192.168.0.41 -p 7001 -a 1111 --cluster create 192.168.0.41:7001 192.168.0.41:7002 192.168.0.41:7003 192.168.0.41:7004 192.168.0.41:7005 192.168.0.41:7006 --cluster-replicas 1
+
